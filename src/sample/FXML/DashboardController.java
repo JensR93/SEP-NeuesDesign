8a65b7c788ffe6e6a54d9ff69bf627db0948d1ba
Tobi-Laptop <Tobias.Sandfort@hs-osnabrueck.de>
2017-09-16 15:18:28 +0200

    erstellung

---
+++ src/sample/FXML/DashboardController.java
@@ -0,0 +1,160 @@
+/**
+ * Sample Skeleton for 'Dashboard.fxml' Controller Class
+ */
+
+package sample.FXML;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXToolbar;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javafx.animation.FadeTransition;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+
+public class DashboardController implements Initializable{
+
+    private AnchorPane home, add, list;
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="stackPane"
+    private StackPane stackPane; // Value injected by FXMLLoader
+
+    @FXML // fx:id="toolBar"
+    private JFXToolbar toolBar; // Value injected by FXMLLoader
+
+    @FXML // fx:id="toolBarRight"
+    private HBox toolBarRight; // Value injected by FXMLLoader
+
+    @FXML // fx:id="sideAnchor"
+    private AnchorPane sideAnchor; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnHome"
+    private JFXButton btnHome; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnTurnierübersicht"
+    private JFXButton btnTurnierübersicht; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnKlassen"
+    private JFXButton btnKlassen; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnSpieler"
+    private JFXButton btnSpieler; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnTeams"
+    private JFXButton btnTeams; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnzeitplan"
+    private JFXButton btnzeitplan; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnStatistik"
+    private JFXButton btnStatistik; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnEinstellungen"
+    private JFXButton btnEinstellungen; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnBeenden"
+    private JFXButton btnBeenden; // Value injected by FXMLLoader
+
+    @FXML // fx:id="holderPane"
+    private AnchorPane holderPane; // Value injected by FXMLLoader
+
+    @FXML
+    void Beenden(ActionEvent event)
+    {
+        Platform.exit();
+    }
+
+    @FXML
+    void einstellungenoeffnen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void homeoeffnen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void klassenoeffnen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void spieleroeffnen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void statistikoeffnen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void teamsoeffnen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void turnierübersichtoeffnen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void zeitplanoeffnen(ActionEvent event) {
+
+    }
+
+    private void createPages() {
+        try {
+            home = FXMLLoader.load(getClass().getResource("test.fxml"));
+            list = FXMLLoader.load(getClass().getResource("test.fxml"));
+            add = FXMLLoader.load(getClass().getResource("test.fxml"));
+
+            //set up default node on page load
+            setNode(home);
+        } catch (IOException ex) {
+            Logger.getLogger(DashboardController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    private void setNode(Node node)
+    {
+        holderPane.getChildren().clear();
+        holderPane.getChildren().add((Node) node);
+        FadeTransition ft = new FadeTransition(Duration.millis(10000));
+        ft.setNode(node);
+        ft.setFromValue(0.1);
+        ft.setToValue(1);
+        ft.setCycleCount(1);
+        ft.setAutoReverse(false);
+        ft.play();
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        createPages();
+
+
+    }
+}
